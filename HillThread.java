0c7e6813b8f8bafd2a049e26d94c68615b4f7bba
sperky7933 <sperky7933@gmail.com>
2023-04-10 13:41:15 -0400

    hillthread set up to begin search algoritm

--- HillThread.java
+++ HillThread.java
@@ -2,4 +2,18 @@
 public class HillThread extends Thread{
     //This is a thread that performs a search using heuristics and such
 
+    String threadname;
+    ThreadGroup group;
+    HillThread(String threadname, ThreadGroup group)
+    {
+        super(group, threadname);
+        this.threadname = threadname;
+        start();
+    }
+
+    public void run()
+    {
+        System.out.println("THREAD " + threadname + " ENABLED. BEGINNING SEARCH.");
+
+    }
 }
